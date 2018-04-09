3503b3c20fbad8725200cbb64825e0fce100c89f
leo <leozhang621@gmail.com>
2018-04-09 10:40:50 +0800

    update

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function $$(selector) {
     return document.querySelectorAll(selector)
 }
 
-let container = $('#container')
+const container = $('#container')
 
 function backgroundColorAnime(){
     anime({
@@ -16,36 +16,9 @@ function backgroundColorAnime(){
             {value:'#E26966'},
         ],
         easing: 'linear',
-        duration:5000,
+        duration: 5000,
         direction:'alternate',
         loop: true,
     })
 }
-
-function toSuccess (){
-    anime({
-        targets:'#messageOutline',
-        d:{
-            value:[
-                'M242.1,265.7l52.4,52.3l51.1-52.3',
-                'M308,312.3l-26.2-26 M281.8,313.3l26.2-27'
-            ]
-        },
-        easing:'easeOutSine',
-        duration: 500,
-    })
-}
-
-
-
-function popAnime(){
-    anime({
-        targets:'#container #popGroup line',
-        strokeDashoffset: [0,anime.setDashoffset],
-        easing:'easeOutSine',
-        duration: 300,
-    })
-}
-toSuccess()
-//popAnime()
-// backgroundColorAnime()
+backgroundColorAnime()
